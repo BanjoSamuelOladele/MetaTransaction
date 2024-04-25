5b1ee9e0a46e113a48be1d0f8c4dca9bc173674d
BanjoSamuelOladele <o.samuel@native.semicolon.africa>
2024-04-25 11:17:50 +0100

    updated

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ require("dotenv").config();
 const {gaslessTransaction, relayerServicePrivateKeyy} = require("./gaslessTransaction");
 
 const express = require('express');
-const app = express()
+const cors = require("cors");
+const morgan = require("morgan");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(morgan("common"));
 app.get("/first", function(req, res) {
     res.send("learning is fun and interesting...");
 });
