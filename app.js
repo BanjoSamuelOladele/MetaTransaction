3e72733339999fc2ff19d677a56ae3ff6fefdef9
Samuel Oladele Banjo <o.samuel@native.semicolon.africa>
2024-05-06 17:06:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,14 +14,21 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("common"));
 
 
+function setUpWallet(provider) {
+    const encryptedJsonKey = fs.readFileSync("./.encryptedKey.json", "utf8");
+    let wallet = ethers.Wallet.fromEncryptedJsonSync(encryptedJsonKey, process.env.PRIVATE_KEY_PASSWORD);
+    wallet = wallet.connect(provider);
+
+    return wallet;
+}
+
 
 async function changeBalance(data) {
     try {
         const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 
-        const encryptedJsonKey = fs.readFileSync("./.encryptedKey.json", "utf8");
-        let wallet = ethers.Wallet.fromEncryptedJsonSync(encryptedJsonKey, process.env.PRIVATE_KEY_PASSWORD);
-        wallet = wallet.connect(provider)
+        
+        let wallet = setUpWallet(provider);
 
         const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS,
             ABI,
